aabf93608569ae9e37da8f2b56c43c61738a73bd
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-23 18:08:38 -0300

    test(light-bulb): add test to check internal method that checks if input is neither 0 or 1

--- src/test/java/org/example/LightBulbManagerTest.java
+++ src/test/java/org/example/LightBulbManagerTest.java
@@ -14,10 +14,25 @@ public class LightBulbManagerTest {
     @Nested
     @DisplayName("Testing invalid equivalence classes")
     class TestingInvalidClasses{
+
+        @Test
+        @DisplayName("Shoul return false when input ins´t either 0 or 1")
+        void ShoulReturnFalseWhenInputInstEitherZeroOr1() {
+            int a = -1;
+            int b = 0;
+            int c = 1;
+            int d = 2;
+            LightBulbManager manager = new LightBulbManager();
+            assertEquals(manager.isInputInvalid(a), false);
+            assertEquals(manager.isInputInvalid(b), true);
+            assertEquals(manager.isInputInvalid(c), true);
+            assertEquals(manager.isInputInvalid(d), false);
+        }
+
         @Test
         @DisplayName("Should return exception when input different from 0 or 1")
         void ShouldReturnExceptionWhenInputDifferentFrom0And1 () {
-            int ia = 2;
+            int ia = -2;
             int ib = 0;
             int fa = 0;
             int fb = 1;
@@ -27,22 +42,6 @@ public class LightBulbManagerTest {
             });
 
             assertEquals("Input must be either 0 or 1", exception01.getMessage());
-
-            ia = -1;
-
-            Exception exception02 = assertThrows(IllegalArgumentException.class, () -> {
-                new LightBulbManager(ia, ib, fa, fb);
-            });
-
-            assertEquals("Input must be either 0 or 1", exception02.getMessage());
-
-            final String newAi = "string";
-
-            Exception exception03 = assertThrows(IllegalArgumentException.class, () -> {
-                new LightBulbManager(newAi, ib, fa, fb);
-            });
-
-            assertEquals("Input must be either 0 or 1", exception02.getMessage());
         }
     }
 }
