ad9d756e630eb26e4c5505758b9bcaf033e9064e
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-23 21:14:53 -0300

    test(light-bulb): added tests to check if the two diferent switchs works rigth

--- src/test/java/org/example/LightBulbManagerTest.java
+++ src/test/java/org/example/LightBulbManagerTest.java
@@ -35,7 +35,7 @@ public class LightBulbManagerTest {
             int ia = -2;
             int ib = 0;
             int fa = 0;
-            int fb = 1;
+            int fb = 100;
 
             Exception exception01 = assertThrows(IllegalArgumentException.class, () -> {
                 new LightBulbManager(ia, ib, fa, fb);
@@ -43,5 +43,42 @@ public class LightBulbManagerTest {
 
             assertEquals("Input must be either 0 or 1", exception01.getMessage());
         }
+
+        @Test
+        @DisplayName("Should only change state of ligth bulb 'A' when clicking the first switch")
+        void ShouldOnlyChangeStateOfLigthBulbAWhenClickingTheFirstSwitch() {
+            LightBulbManager manager = new LightBulbManager();
+            LightBulbManager.setLightBulbA(0);
+            LightBulbManager.clickFirstSwitch();
+            assertEquals(LightBulbManager.getLightBulbA, 1);
+            LightBulbManager.clickFirstSwitch();
+            assertEquals(LightBulbManager.getLightBulbA, 0);
+        }
+
+        @Test
+        @DisplayName("Should change state of the two ligth bulbs when clicking the second switch")
+        void ShouldChangeStateOfLigthBulbsWhenClickingTheSecondSwitch() {
+            LightBulbManager manager = new LightBulbManager();
+            LightBulbManager.setLightBulbA(0);
+            LightBulbManager.setLightBulbB(1);
+            LightBulbManager.clickSecondSwitch();
+            assertEquals(LightBulbManager.getLightBulbA, 1);
+            assertEquals(LightBulbManager.getLightBulbB, 0);
+
+            LightBulbManager.clickSecondSwitch();
+            assertEquals(LightBulbManager.getLightBulbA, 0);
+            assertEquals(LightBulbManager.getLightBulbB, 1);
+
+            LightBulbManager.setLightBulbA(1);
+            LightBulbManager.setLightBulbB(1);
+            LightBulbManager.clickSecondSwitch();
+            assertEquals(LightBulbManager.getLightBulbA, 0);
+            assertEquals(LightBulbManager.getLightBulbB, 0);
+
+            LightBulbManager.clickSecondSwitch();
+            assertEquals(LightBulbManager.getLightBulbA, 1);
+            assertEquals(LightBulbManager.getLightBulbB, 1);
+        }
+
     }
 }
